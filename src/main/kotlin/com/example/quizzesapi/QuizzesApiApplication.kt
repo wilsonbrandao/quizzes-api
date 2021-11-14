16f923d0e78e27829659a381aaf476f97a45d248
Wilson Brandão <wilsonbrandaocosta@gmail.com>
2021-11-14 19:44:54 -0300

    Commit initial

---
+++ src/main/kotlin/com/example/quizzesapi/QuizzesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quizzesapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizzesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizzesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/quizzesapi/QuizzesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quizzesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizzesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
