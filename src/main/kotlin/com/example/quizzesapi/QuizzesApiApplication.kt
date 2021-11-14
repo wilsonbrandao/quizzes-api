a5390d833262e97eb0fbf8f46482426718bce416
Wilson Brandão <wilsonbrandaocosta@gmail.com>
2021-11-14 20:03:32 -0300

    testando auto deploy do heroku

--- src/main/kotlin/com/example/quizzesapi/QuizzesApiApplication.kt
+++ src/main/kotlin/com/example/quizzesapi/QuizzesApiApplication.kt
@@ -2,10 +2,20 @@ package com.example.quizzesapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class QuizzesApiApplication
 
 fun main(args: Array<String>) {
 	runApplication<QuizzesApiApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+	@GetMapping
+	fun test() = "test"
 }
